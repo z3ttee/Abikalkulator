b3e8a9beff1b135c6edddece84959d3162657ca8
z3ttee <cedric.zitzmann@gmail.com>
2020-03-30 20:09:46 +0200

    Added share functionality

--- app/src/main/java/de/zitzmanncedric/abicalc/fragments/main/SettingsFragment.java
+++ app/src/main/java/de/zitzmanncedric/abicalc/fragments/main/SettingsFragment.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 
 import de.zitzmanncedric.abicalc.AppCore;
+import de.zitzmanncedric.abicalc.BuildConfig;
 import de.zitzmanncedric.abicalc.R;
 import de.zitzmanncedric.abicalc.activities.settings.SettingsGoalsActivity;
 import de.zitzmanncedric.abicalc.activities.settings.SettingsSeminarActivity;
@@ -43,15 +44,13 @@ public class SettingsFragment extends Fragment implements SettingsListAdapter.Ca
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         RecyclerView list = view.findViewById(R.id.recyclerview_settings);
 
         ArrayList<SettingsItem> items = new ArrayList<>();
         items.add(new SettingsItem(R.string.settings_seminar, R.string.settings_seminar_desc, view.getContext().getDrawable(R.drawable.ic_student)));
         items.add(new SettingsItem(R.string.settings_subjects, R.string.settings_subjects_desc, view.getContext().getDrawable(R.drawable.ic_schedule)));
         items.add(new SettingsItem(R.string.settings_goals, R.string.settings_goals_desc, view.getContext().getDrawable(R.drawable.ic_award)));
-        // TODO: Share option
-        // items.add(new SettingsItem(R.string.settings_share, R.string.settings_share_desc, view.getContext().getDrawable(R.drawable.ic_share)));
+        items.add(new SettingsItem(R.string.settings_share, R.string.settings_share_desc, view.getContext().getDrawable(R.drawable.ic_share)));
         items.add(new SettingsItem(R.string.settings_privacy, R.string.settings_privacy_desc, view.getContext().getDrawable(R.drawable.ic_shield)));
         items.add(new SettingsItem(R.string.settings_reset, R.string.settings_reset_desc, view.getContext().getDrawable(R.drawable.ic_refresh)));
 
@@ -74,6 +73,13 @@ public class SettingsFragment extends Fragment implements SettingsListAdapter.Ca
             case R.string.settings_goals:
                 Intent goals = new Intent(getContext(), SettingsGoalsActivity.class);
                 startActivity(goals);
+                break;
+            case R.string.settings_share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.paragraph_sharewith).replace("%url%", "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID));
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
                 break;
             case R.string.settings_privacy:
                 String url = "https://zitzmann-cedric.de/projects/abikalkulator/privacy/";
