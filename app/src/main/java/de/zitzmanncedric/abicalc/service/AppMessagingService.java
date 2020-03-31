c97abb40d2866a712e91dd1d7be08bdcd00a8093
z3ttee <cedric.zitzmann@gmail.com>
2020-03-31 20:07:11 +0200

    Added documentation

--- app/src/main/java/de/zitzmanncedric/abicalc/service/AppMessagingService.java
+++ app/src/main/java/de/zitzmanncedric/abicalc/service/AppMessagingService.java
@@ -5,8 +5,15 @@ import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+/**
+ * Hintergrund-Service zum Empfangen von Nachrichten
+ */
 public class AppMessagingService extends FirebaseMessagingService {
 
+    /**
+     * Funktion zum Empfangen von Nachrichten. (Aktuell nicht funktionsfähig, da für spätere Entwicklung gedacht)
+     * @param remoteMessage Empfanges Nachricht-Objekt
+     */
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
